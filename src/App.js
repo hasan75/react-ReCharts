4f124de19db9193fd62ce698299c8bafaec50788
Hasan75 <hasanjab14@gmail.com>
2021-09-30 14:55:07 +0600

    pie chart added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import MyLineChart from './components/MyLineChart/MyLineChart';
 import MyAreaChart from './components/MyAreaChart/MyAreaChart';
+import MyPieChart from './components/MyPieChart/MyPieChart';
 
 function App() {
   return (
     <div className="App">
       <MyLineChart></MyLineChart>
       <MyAreaChart></MyAreaChart>
+      <MyPieChart></MyPieChart>
     </div>
   );
 }
