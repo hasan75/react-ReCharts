5c71aaa994879df53be0d2d59f08195f27f1c837
Hasan75 <hasanjab14@gmail.com>
2021-09-30 14:37:11 +0600

    created a line chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyLineChart from './components/MyLineChart/MyLineChart';
+import MyAreaChart from './components/MyAreaChart/MyAreaChart';
 
 function App() {
   return (
     <div className="App">
+      <MyLineChart></MyLineChart>
       
     </div>
   );
